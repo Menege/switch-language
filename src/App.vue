d76258de86ce875753059bbbb6635c9a40db8aef
user <it.efrem94@yandex.ru>
2021-08-10 13:52:13 +0300

    refresh

--- src/App.vue
+++ src/App.vue
@@ -1,107 +1,34 @@
 <template>
-  <div class="container column">
-    <app-form @sendInfo='sendInfo'></app-form>
-
-    <div class="card card-w70">
-
-      <app-title :title='title'></app-title>
-
-      <app-ava :link="link"></app-ava>
-
-      <app-discr :subtitle="subtitle"></app-discr>
-
-      <app-text :text="text"></app-text>
-
-     <first-word v-if="isFull"></first-word>
-      
-    </div> 
-  </div>
   <div class="container">
-    <p>
-      <button class="btn primary" @click="loadComments">Загрузить комментарии</button>
-    </p>
-    <app-loader v-if="loading"></app-loader>
-    <app-comments v-if="onCommit" :comments='comments'></app-comments>
+    <app-alert
+    v-if="alert"
+text="Тот самый текст"
+title="ССССССССССССВАП"
+type=" warning"
+closable
+@close="alert=false"
+    ></app-alert>
+    <div class="card">
+<button 
+
+class="btn primary" @click="toogleAlert">{{ alert ?'скрыть':'показать'}} сообщение</button>
+    </div>
   </div>
 </template>
-
-<script>
-import AppTitle from './AppTitle.vue'
-import AppAva from './AppAva.vue'
-import AppDiscr from './AppDiscr.vue'
-import AppText from './AppText.vue'
-import FirstWord from './FirstWord.vue'
-import AppForm from './AppForm.vue'
-import AppComments from './AppComments.vue'
-import AppLoader from './AppLoader.vue'
-import axios from 'axios'
-export default {
-  data(){
-    return {
-      title:'',
-      text:'',
-      subtitle:'',
-      onCommit:false,
-      loading:false,
-      link: '',
-      comments:[]    }
-  },
-  components:{AppTitle, AppAva, AppDiscr, AppText, FirstWord, AppForm,AppComments,AppLoader},
-  methods:{
  
-   loadComments() {
-     this.loading=true
-     setTimeout(async () =>{
-try {
-        const {data}=await axios.get('https://jsonplaceholder.typicode.com/comments?_limit=42')
-   if (!data) {
-     throw new Error('Список комментариев пуст')
-   }
-   this.comments= Object.keys(data).map(key=>{
-     return {
-       id:key,
-       ...data[key]
-     }
-   })
-   this.loading=false
-   if (!this.onCommit) return this.onCommit=true
-      } catch (e) {
-        this.alert={
-          type:'danger',
-          title:'Ошибка',
-          text:e.message
-        }
-        this.loading=false
-        console.log(e.message)
-      }
-     }, 1500)
-},
-sendInfo(textArea,selected){
-if (selected==='title') {this.title=textArea}
-else if (selected==='subtitle') {this.subtitle=textArea}
-else if (selected==='avatar') {this.link=textArea}
-else if (selected==='text') { this.text=textArea}
-else alert('Выберите тип блока')
-}},
-  computed:{
-    isFull(){
-      if (this.title.length ===0&&this.subtitle.length ===0&&this.text.length ===0&&this.link.length ===0)
- return true  
-      return false
-      }
-    }
-  } 
+<script>
+import AppAlert from './components/AppAlert.vue'
+export default {
+  data(){return{
+    alert:false
+  }},
+  methods:{
+toogleAlert(){
+  this.alert=!this.alert
+}
+  },
+  
+components:{AppAlert}
+}
 </script>
 
-<style>
-  .avatar {
-    display: flex;
-    justify-content: center;
-  }
-
-  .avatar img {
-    width: 150px;
-    height: auto;
-    border-radius: 50%;
-  }
-</style>
---
+++ src/components/AppAlert.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="alert" :class="type">
+      <p class="alert-title"> {{title}} </p>
+      <p> {{text}} </p>
+      <button class="btn" :class="type" v-if="closable" @click="$emit('close')">Закрыть</button>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+title:String,
+text  :String,
+closable:{
+type:Boolean,
+required:false,
+default:false
+},
+type: {
+    type:String,
+    required:false,
+    default:'primary',
+    validator (val){
+        return ['primary','danger', 'warning'].includes(val)
+    }
+}
+    },
+    emits:['close']
+        
+
+}
+</script>
+
+<style>
+
+</style>
